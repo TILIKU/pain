7b135428ff6360e4e37f3c9f4c0cef0dca412a68
TILIKU <85792875+TILIKU@users.noreply.github.com>
2023-01-20 21:51:55 +0400

    added icons and stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import classes from "../css/Footer.module.css"
 import "../css/Footer.module.css"
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faTwitter, faInstagram, faGooglePlus } from '@fortawesome/free-brands-svg-icons'
+
 
 function Footer(props){
     return(
@@ -9,10 +12,10 @@ function Footer(props){
         <h3>Copyright &copy; 2022. All rights reserved</h3>
     </div>
     <div className={classes.footer_icons}>
-        <a href="#" className={classes.fa}>facebook</a>
-        <a href="#" className={classes.fa}>instagram</a>
-        <a href="#" className={classes.fa}>twitter</a>
-        <a href="#" className={classes.fa}>google</a>
+        <a href="#" className={classes.fa}> <FontAwesomeIcon icon={faFacebook} /></a>
+        <a href="#" className={classes.fa}><FontAwesomeIcon icon={faTwitter} /></a>
+        <a href="#" className={classes.fa}><FontAwesomeIcon icon={faInstagram} /></a>
+        <a href="#" className={classes.fa}><FontAwesomeIcon icon={faGooglePlus} /></a>
     </div>
 </footer>
     )
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,7 +2,10 @@ import classes from "./MainPage.module.css"
 
 function MainPage() {
     return (
-       <h1 className={classes.h1}>main page</h1>
+        <div>
+         <h1 className={classes.h1}>main page</h1>
+         <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dpyramid&psig=AOvVaw0YV3uVgg6wClNBWoRgdvzO&ust=1674321436880000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIiRx6LW1vwCFQAAAAAdAAAAABAI" />
+       </div>
     )
 }
 
