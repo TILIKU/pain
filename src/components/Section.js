58cc7acf179cd092b6e7d841993ddaa43de0cfd2
TILIKU <85792875+TILIKU@users.noreply.github.com>
2023-01-27 13:38:30 +0400

    added flexbox and fixed composition of a page

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,10 +4,8 @@ import classes from "../css/Section.module.css"
 function Section(props){
     return(
     <div className={classes.mainDiv}>
-       
             <div className={classes.sectionText}>text mmmmmmmm</div>
-            <div className={classes.sectionImg}>image?</div>
-        
+            <div className={classes.sectionImg}>image?</div> 
     </div>
     )
 }
